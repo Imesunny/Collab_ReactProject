f0d108f65d538522b2b3e22cbdec4a898f86e38f
ROHIT KHARWAR <rohitkumar27699@gmail.com>
2023-09-13 04:48:45 +0530

    almost done

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Box,
   Button,
@@ -9,16 +9,72 @@ import {
   FormLabel,
   Heading,
   Stack,
-  Center, // Import Center component
+  Center,
+  CloseButton,
+  AlertTitle,
+  AlertIcon,
+  Alert,
+  Flex, // Import Center component
 } from '@chakra-ui/react';
 import background from './1.jpg';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 const Login = () => {
-
-
   const nav=useNavigate()
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [errorText, setErrorText] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!email) {
+      setShowErrorMessage(true);
+      setErrorText('Email required');
+      return;
+    }
+    if (!password) {
+      setShowErrorMessage(true);
+      setErrorText('Password are required');
+      return;
+    }
+
+
+    try {
+      // Check if the email exists
+      const emailExistsResponse = await axios.get(
+        `http://localhost:3000/users?email=${email}`
+      );
+
+      if (emailExistsResponse.data.length === 0) {
+        setShowErrorMessage(true);
+        setErrorText('User not found');
+        return;
+      }
+
+      // Check if the password matches
+      const user = emailExistsResponse.data[0];
+      if (user.password !== password) {
+        setShowErrorMessage(true);
+        setErrorText('Password does not match');
+        return;
+      }
+
+      setShowSuccessMessage(true);
+      setShowErrorMessage(false);
+      setErrorText('');
+      // Save user data here if needed
+      console.log('User data:', user);
+    } catch (error) {
+      console.error('Login error:', error);
+      setShowErrorMessage(true);
+      setErrorText('Login failed. Please try again.');
+    }
+  };
   return (
     <ChakraProvider >
       <CSSReset />
@@ -39,23 +95,41 @@ const Login = () => {
           borderRadius={4}
           color='white'
           className='main_box'
-          width='30%'
+          width='40%'
           minW="40vh"
 
         >
           <Box textAlign="center">
             <Heading color="#C68409">Login</Heading>
           </Box>
+          {showErrorMessage && (
+            <Alert status="error" mt={9} backgroundColor='red'>
+              <AlertIcon />
+              <AlertTitle>{errorText}</AlertTitle>
+              <CloseButton onClick={() => setShowErrorMessage(false)} />
+            </Alert>
+          )}
+          {showSuccessMessage && (
+            <Alert status="success" mt={4} backgroundColor='green'>
+              <AlertIcon />
+              <AlertTitle >Login successful!</AlertTitle>
+              <CloseButton onClick={() => setShowSuccessMessage(false)} />
+            </Alert>
+          )}
           <Box my={4} textAlign="left">
-            <form>
+            <form onSubmit={handleSubmit}>
               <Stack spacing={4}>
                 <FormControl>
-                  <FormLabel>Email or Username</FormLabel>
-                  <Input type="text" placeholder="Enter your email or username" />
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" placeholder="Enter your email" 
+                  value={email}
+                  onChange={(e)=>setEmail(e.target.value)}/>
                 </FormControl>
                 <FormControl>
                   <FormLabel>Password</FormLabel>
-                  <Input type="password" placeholder="Enter your password" />
+                  <Input type="password" placeholder="Enter your password" 
+                   value={password}
+                   onChange={(e)=>setPassword(e.target.value)}/>
                 </FormControl>
                 <Center> {/* Wrap the button with the Center component */}
                   <Button
@@ -72,12 +146,11 @@ const Login = () => {
                 </Center>
                 <Center _hover={{ bg: 'gray' }}
                   _active={{ bg: 'purple' }}>
-                  Forgot your PassWord
+                  Forgot your Password
                 </Center>
-                <Center
-                onClick={()=> nav("/signup")}
-                 _hover={{ bg: 'gray' }}
-                  _active={{ bg: 'purple' }}>Do not have an Account? Create yours now.</Center>
+                <Center>
+                 <Flex flexDir='column'><div>Do not have an Account ?</div><button
+                onClick={()=> nav("/signup")} style={{color:'orange'}}> Create yours now.</button></Flex> </Center>
 
 
               </Stack>
