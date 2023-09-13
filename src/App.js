a0d632273292ce277d76b3ae038532fb351a29b3
unknown <hiteshsonawane900@gmail.com>
2023-09-13 12:00:27 +0530

    payment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AlllRoutes from './Routes/AlllRoutes';
 
 function App() {
   return (
     <div className="App">
-      Login Page
+      <AlllRoutes/>
     </div>
   );
 }
---
+++ src/Routes/AlllRoutes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AlllRoutes = () => {
+  return (
+    <div>AlllRoutes</div>
+  )
+}
+
+export default AlllRoutes
